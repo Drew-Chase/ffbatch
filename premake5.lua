63769cc077009af6143ca3131e05acfc73cfb8cf
Drew Chase <dcmanproductions@gmail.com>
2023-11-21 01:20:33 -0500

    added example configuration

--- premake5.lua
+++ premake5.lua
@@ -19,6 +19,12 @@ project (name)
     cppdialect "C++20"
     cdialect "C17"
     characterset "Unicode"
+    
+    filter "configurations:Example"
+        defines { "EXAMPLE" }
+        targetdir "bin/Example/"
+        objdir "bin/obj/example"
+        staticruntime "Off"
 
     filter "configurations:Dynamic Library"
         kind "SharedLib"
@@ -69,6 +75,37 @@ project "example"
         targetdir "bin/Example/"
         objdir "bin/obj/example"
         staticruntime "Off"
+
+    filter "configurations:Dynamic Library"
+        kind "SharedLib"
+        defines { "DYNAMIC_LIBRARY" }
+        targetdir "bin/build/bin"
+        staticruntime "Off"
+        objdir "bin/obj/dll"
+        
+    filter "configurations:Static Library Debug"
+        kind "StaticLib"
+        defines { "STATIC_LIBRARY_DEBUG" }
+        symbols "On"
+        targetextension "-d.lib"
+        targetdir "bin/build/lib"
+        staticruntime "On"
+        objdir "bin/obj/lib-d"
+        
+    filter "configurations:Static Library"
+        kind "StaticLib"
+        defines { "STATIC_LIBRARY" }
+        staticruntime "On"
+        targetextension ".lib"
+        targetdir "bin/build/lib"
+        objdir "bin/obj/lib"
+
+    filter "platforms:Win32"
+        architecture "x86"
+
+    filter "platforms:x64"
+        architecture "x86_64"
         
     filter {} -- reset filter
     files { "example/**/*.rc", "example/**/*.h", "example/**/*.cpp" }
+    includedirs { name .. "/includes", "include" } -- Add "include" directory
