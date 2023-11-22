489d05dc40a2e953d0f8ce41b5ed2074a4df408a
Drew Chase <dcmanproductions@gmail.com>
2023-11-22 09:24:42 -0500

    added test class

---
+++ project/includes/test.h
@@ -0,0 +1,7 @@
+// LFInteractive LLC. (c) 2020-2024 All Rights Reserved
+#pragma once
+class test
+{
+public:
+	static void hi();
+};
---
+++ project/src/test.cpp
@@ -0,0 +1,8 @@
+// LFInteractive LLC. (c) 2020-2024 All Rights Reserved
+#include "test.h"
+#include <iostream>
+
+void test::hi()
+{
+	std::cout << "hi" << std::endl;
+}
