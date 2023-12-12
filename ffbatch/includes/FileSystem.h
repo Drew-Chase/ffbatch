33dd333658fb7f3ff19079264e4e0aa3dfae3fe5
Drew Chase <dcmanproductions@gmail.com>
2023-12-11 21:19:17 -0500

    Add FileSystem class with necessary functions

---
+++ ffbatch/includes/FileSystem.h
@@ -0,0 +1,15 @@
+// LFInteractive LLC. (c) 2020-2024 All Rights Reserved
+#pragma once
+
+#include <string>
+#include <vector>
+using std::vector;
+using std::string;
+
+class FileSystem
+{
+public:
+	static void Parse(string& path, bool& recursive, vector<string>& extensions);
+	static vector<string> GetFilesInDirectory(string directory, vector<string> extension, bool recursive);
+	static vector<string> GenerateCommands(string command,vector<string> inputs, vector<string> args);
+};
