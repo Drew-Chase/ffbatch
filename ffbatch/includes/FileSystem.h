5c72e6d653220fcd7014acb953491856db418e3b
drew-chase <dcmanproductions@gmail.com>
2023-12-12 15:30:21 -0500

    Refactor FileSystem class and add GetFFmpegExecutable method

--- ffbatch/includes/FileSystem.h
+++ ffbatch/includes/FileSystem.h
@@ -6,10 +6,14 @@
 using std::vector;
 using std::string;
 
-class FileSystem
+namespace ffbatch
 {
-public:
-	static void Parse(string& path, bool& recursive, vector<string>& extensions);
-	static vector<string> GetFilesInDirectory(string directory, vector<string> extension, bool recursive);
-	static vector<string> GenerateCommands(string command,vector<string> inputs, vector<string> args);
-};
+	class FileSystem
+	{
+	public:
+		static void Parse(string& path, bool& recursive, vector<string>& extensions);
+		static vector<string> GetFilesInDirectory(string directory, vector<string> extension, bool recursive);
+		static vector<string> GenerateCommands(string command, vector<string> inputs, vector<string> args);
+		static bool GetFFmpegExecutable(string& path);
+	};
+}
